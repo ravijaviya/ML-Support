4f2c4fb54b1953cf89323e0d9a72ac89f5c1672b
NotoriouslySuave <sairam.pillai61@gmail.com>
2017-01-31 12:42:01 +0530

    Rapid Automatic Keyword Extraction Experiments (RAKE)

---
+++ RAKEtrial.py
@@ -0,0 +1,12 @@
+from rake_nltk import  Rake
+
+with open('plot_the_big_short','r') as p:
+
+    rake = Rake()
+    rake.extract_keywords_from_sentences(p)
+    total_keys = 0
+    for score,keyword in rake.get_ranked_phrases_with_scores():
+        print(score,keyword)
+        if score > 1:
+            total_keys+=len(keyword.split(" "))
+    print(total_keys,len(rake.get_ranked_phrases()))
